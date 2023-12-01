6a08e288ebc015cbd0474982d5c8658390d75f6f
GuilhermeEstevan <guilhermeestevangui@hotmail.com>
2023-12-01 11:46:05 -0300

    raw: endereço http

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const customFetch = axios.create({
-  baseURL: "https://15.228.54.44:3000/api/v1",
+  baseURL: "http://15.228.54.44:3000/api/v1",
 });
 
 export default customFetch;
