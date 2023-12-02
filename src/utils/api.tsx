47eb9cfc85e0034139a53d1fb1e72c5293badedb
GuilhermeEstevan <guilhermeestevangui@hotmail.com>
2023-12-02 14:26:26 -0300

    fix: dominio

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const customFetch = axios.create({
-  baseURL: "http://15.228.54.44:3000/api/v1",
+  baseURL: "https://www.petlovers.app.br:3000/api/v1",
 });
 
 export default customFetch;
