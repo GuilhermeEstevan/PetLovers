bc41efe645eafb07cb71afa7e7fe22df1daa9399
GuilhermeEstevan <guilhermeestevangui@hotmail.com>
2023-12-05 17:29:25 -0300

    fix: build details

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./assets/css/index.css";
 import { UserProvider } from "./context/userContext.tsx";
 import { PetProvider } from "./context/petContext.tsx";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById("root")!).render(
   <UserProvider>
     <PetProvider>
       <App />
